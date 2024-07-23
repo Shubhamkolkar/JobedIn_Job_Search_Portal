dfc2c99ddf335586314460886c8bd6e71f30fc13
Shubham Kolkar <shubhamkolkar03@gmail.com>
2024-07-23 10:59:23 +0530

    Started with backed

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+
+import express, { urlencoded } from "express";
+import connectDB from "./db/connection.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import bodyParser from "body-parser";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+ 
+
+
+
+
+
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+
+// middleware
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:"http://localhost:5173",
+    credentials:true
+}
+app.use(cors(corsOptions));
+
+app.listen(PORT, () => {
+    console.log(`server running at port ${PORT}`);
+});
